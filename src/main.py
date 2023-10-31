126ec09cd29aee53fd5cb5cd8b291fbd78338169
DrakkuN <dracnguyenbku@gmail.com>
2023-10-31 15:02:48 +0700

    continue select file and sender

--- src/main.py
+++ src/main.py
@@ -4,15 +4,57 @@ from tkinter import  filedialog
 from tkinter import messagebox
 import os
 
-import sel #select_file code
-import se  #send code
-import re  #receive code
-
 root  = TK()
 root.title("Shareit")
 root.geometry("450x560+500+200")
 root.configure(bg= "#f4fdfe")
 root.resizable(False,False)
+
+def Send():
+    window=Toplevel(root)
+    window.title("Send")
+    window.geometry("450x560+500+200")
+    window.configure(bg="#f4fdfe")
+    window.resizable(False,False)
+
+    def select_file():
+        #continue
+    def sender():
+        #continue
+
+    #icon
+    image_icon1=PhotoImage(file="Image/send.png")
+    window.iconphoto(False,iamge_icon1)
+    
+    Sbackground=PhotoImage(file="Image/sender.png")
+    Label(window,image=Sbackground).place(x=-2,y=0)
+
+    Mbackground=PhotoImage(file="Image/id.png")
+    Label(window,image=Mbackground,bg='#f4fdfe').place(x=100,y=260)
+
+    host=socket.gethostname()
+    Label(window,text=f'ID:{host}',bg='white',fg='black').place(x-140,y-290)
+
+    Button(window,text="+ select file",width=10,height =1 ,font='arial 14 bold', bg="#fff",fg="#000",command=select_file).place(x=160,y=150)
+    Button(window,text="SEND",width=8,height=1,font='arial 14 bold', bg="#000",fg="#fff").place(x=300,y=150)
+
+    window.mainloop()
+
+
+def Receive():
+    main=Toplevel(root)
+    main.title("Receive")
+    main.geometry("450x560+500+200")
+    main.configure(bg="#f4fdfe")
+    main.resizable(False,False)
+
+
+    #icon
+    image_icon1=PhotoImage(file="Image/receive.png")
+    main.iconphoto(False,iamge_icon1)
+
+    main.mainloop()
+
 
 #icon
 image_icon=PhotoImage(file="Image/icon.png")
