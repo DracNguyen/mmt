bd4f47e6989c5f9fa3d4758bd391cc3a7fb014de
DrakkuN <dracnguyenbku@gmail.com>
2023-11-01 14:09:51 +0700

    done P2P

--- src/main.py
+++ src/main.py
@@ -18,9 +18,23 @@ def Send():
     window.resizable(False,False)
 
     def select_file():
-        #continue
+        global filename
+        filename=filedialog.askopenfilename(initialdir=os.getcwd(),title='Select Image File',filetype=(('file_type','*.txt'),('all files','*.*')))
+        #icon
+        Button(window,text="SEND",width=8,height=1,font='arial 14 bold',bg='#000',fg="#fff",command=sender).place(x=300,y=150)
     def sender():
-        #continue
+        s=socket.socket()
+        host=socket.gethostname()
+        port=8080
+        s.bind((host,port))
+        s.listen(1)
+        print(host)
+        print('waiting for any incoming connections....')
+        conn,addr=s.accept()
+        file=open(filename,'rb')
+        file_data=file.read(1024)
+        conn.send(file_data)
+        print("Data has been transmitted successfully..")
 
     #icon
     image_icon1=PhotoImage(file="Image/send.png")
@@ -42,17 +56,49 @@ def Send():
 
 
 def Receive():
-    main=Toplevel(root)
+    main = Toplevel(root)
     main.title("Receive")
-    main.geometry("450x560+500+200")
+    main.geometry('450x560+500+200')
     main.configure(bg="#f4fdfe")
-    main.resizable(False,False)
+    main.resizeable(False, False)
+    
+    def receiver():
+        ID = SenderID.get()
+        filename1 = incoming_file.get()
+        
+        s=socket.socket()
+        port=8080
+        s.connect((ID,port))
+        file = open(filename1,'wb')
+        file_data = s.recv(1024)
+        file.write(file_data)
+        file.close()
+        print("File has been received successfully")
 
+    # icon
+    image_icon1 = PhotoImage(file="Image/receive.png")
+    main.iconphoto(False, image_icon1)
 
-    #icon
-    image_icon1=PhotoImage(file="Image/receive.png")
-    main.iconphoto(False,iamge_icon1)
+    Hbackground = PhotoImage(file="Image/receiver.png")
+    Label(main, image=Hbackground).place(x=-2, y=0)
 
+    logo = PhotoImage(file='Image/profile.png')
+    Label(main, image=logo, bg="f4fdfe").place(x=10, y=250)
+
+    Label(main, text = "Receive", font = ('arial', 20), bg=f4fdfe).place(x=100,y=280)
+    
+    Label(main, text = "Input sender id", font = ('arial', 10, 'bold'), bg ="f4fdfe").place(x=20,y=340)
+    SenderID = Entry(main, width=25, fg="black", border=2, bg='while', font=('arial', 15))
+    SenderID.place(x=20, y=370)
+    SenderID.focus()
+    
+    Label(main, text = "filename for the incoming file:", font = ('arial', 10, 'bold'), bg ="f4fdfe").place(x=20,y=420)
+    incoming_file = Entry(main, width=25, fg="black", border=2, bg='while', font=('arial', 15))
+    incoming_file.place(x=20, y=450)
+    
+    imageicon = PhotoImage(file="Image/arrow.png")
+    rr = Button(main,text="Receive", compound=LEFT, image=imageicon, width=130, bg="#39c790", font="arial 14 bold", command = receiver)
+    rr.place(x=20,y=500)
     main.mainloop()
 
 
